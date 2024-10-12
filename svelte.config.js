66f35a5181ba3abf28595a37edf3c1466048fdfe
Kiruthik <147307440+kiruthikpurpose@users.noreply.github.com>
2024-10-12 20:54:11 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,16 +4,16 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
+			// Fallback page for unmatched routes
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			// Dynamically set base path depending on the environment
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
 		}
 	},
+	// Preprocessing with Vite
 	preprocess: vitePreprocess()
 };
 
